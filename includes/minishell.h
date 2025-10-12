1e2573448bc5b778c14f5a654a5bc839db137208
Amine Wahab <amwahab@f2r2s7.clusters.42paris.fr>
2025-10-12 11:53:44 +0200

    first commit

---
+++ includes/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amwahab <amwahab@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/02 17:50:03 by amwahab           #+#    #+#             */
+/*   Updated: 2025/10/02 17:57:44 by amwahab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
