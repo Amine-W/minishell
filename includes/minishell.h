5c3f288b6194257f1da25a89d779aa9a4b1c1110
Amine-W <aminewahabb@gmail.com>
2025-10-12 15:48:02 +0200

    feat: implement readline loop with history and EOF handling

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amwahab <amwahab@student.42.fr>            +#+  +:+       +#+        */
+/*   By: amwahab <amwahab@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 17:50:03 by amwahab           #+#    #+#             */
-/*   Updated: 2025/10/02 17:57:44 by amwahab          ###   ########.fr       */
+/*   Updated: 2025/10/12 15:27:48 by amwahab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -3,22 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amwahab <amwahab@student.42.fr>            +#+  +:+       +#+        */
+/*   By: amwahab <amwahab@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 17:49:44 by amwahab           #+#    #+#             */
-/*   Updated: 2025/10/12 12:42:37 by amwahab          ###   ########.fr       */
+/*   Updated: 2025/10/12 15:35:41 by amwahab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(void)
+int	main(void)
 {
 	char	*line;
 
-	while(1)
+	while (1)
 	{
-		line = readline("minicheh>");
+		line = readline("minishell> ");
+		if (line == NULL)
+		{
+			printf("exit\n");
+			rl_clear_history();
+			break ;
+		}
+		else if (line[0] == '\0')
+		{
+			free(line);
+			continue ;
+		}
+		add_history(line);
+
+		//traiter la commande pour plus tard//
+		printf("vous avez tapé: %s\n", line);
+		//traiter la commande pour plus tard//
 		free(line);
 	}
+	return (0);
 }
